3711973957cb1cc9f3f3948afbe1882a28d39016
ato-z <523333404@qq.com>
2022-12-28 14:47:24 +0800

    🥔 迭代: 創建運行時目錄

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,18 @@
+import { resolve, join } from 'path';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { staticConfig } from '@config/static';
+import { touchPath } from 'tool';
+
+const rootPath = resolve(__dirname, '../');
+staticConfig.root = rootPath;
+
+/**創建運行時目錄 */
+touchPath(join(rootPath, staticConfig.runtime));
+touchPath(join(rootPath, staticConfig.runtimeCache));
+touchPath(join(rootPath, staticConfig.runtimeLog));
+touchPath(join(rootPath, staticConfig.runtimeToken));
+
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     await app.listen(3000);
