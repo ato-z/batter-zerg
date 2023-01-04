177eaa1b75160cb23c36e5b7d5b6733115da1add
ato-z <523333404@qq.com>
2023-01-04 20:24:04 +0800

    🍒 新增: 商品模块

---
+++ src/database/good.cate.databse.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@nestjs/common';
+import { BaseModel } from './base.database';
+
+export type GoodsCateBase = {
+    id: number;
+    title: string;
+    name: string;
+    pid: number;
+    pic: number;
+    order: number;
+    create_date: string;
+    delete_date: string | null;
+};
+@Injectable()
+export class GoodsCateModel extends BaseModel<GoodsCateBase> {
+    protected tableName = 'goods_cate';
+}
