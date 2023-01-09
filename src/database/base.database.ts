9395df08c150eccbc7704924a13b6e7316fa2104
ato-z <523333404@qq.com>
2023-01-09 22:27:04 +0800

    🍒 新增: SelectOption类型

--- src/database/base.database.ts
+++ src/database/base.database.ts
@@ -5,6 +5,12 @@ import DataBaseModel from 'mysql-crud-model';
 const { host, port, password, database, user, table_prefix } = databaseConfig;
 const pool = createPool({ host, port, password, database, user });
 
+export type SelectOption<T extends BaseModel<any>['selete']> = T extends (
+    o: infer O,
+) => unknown
+    ? O
+    : never;
+
 export class BaseModel<T> extends DataBaseModel<T> {
     protected prefix: string = table_prefix;
     constructor() {
