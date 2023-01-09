7998bde9e44f1ecc263d303fafe29b7437d1e5d9
ato-z <523333404@qq.com>
2023-01-09 22:24:51 +0800

    🍒 新增: 訂單模塊接口

---
+++ src/database/goods.order.database.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@nestjs/common';
+import { json } from 'stream/consumers';
+import { BaseModel } from './base.database';
+
+export type GoodsOrderBase = {
+    id: number;
+    order_no: string;
+    from_uid: number;
+    from_goods_id: number;
+    from_address: string;
+    to_uid: number;
+    to_goods_id: number;
+    to_address: string;
+    status: number;
+    create_date: string;
+    delete_date: string | null;
+};
+
+const deCodeAddressJSON = (json: string) => {
+    try {
+        const data = JSON.parse(json);
+        return data;
+    } catch {
+        return json;
+    }
+};
+
+@Injectable()
+export class GoodsOrderModel extends BaseModel<GoodsOrderBase> {
+    protected tableName = 'goods_order';
+
+    getting = {
+        from_address: (json: string) => deCodeAddressJSON(json),
+        to_address: (json: string) => deCodeAddressJSON(json),
+    };
+}
