8bf70710b6538239fb9d2f71abc6f35e6e2b82ef
ato-z <523333404@qq.com>
2023-01-12 21:53:34 +0800

    🍌 修改: 儅七牛雲回調地址無法訪問時不進行上傳

--- src/modules/common/qiniu/qiniu.service.ts
+++ src/modules/common/qiniu/qiniu.service.ts
@@ -1,11 +1,7 @@
 import { staticConfig } from '@config/static';
-import { ConfigModel } from '@database/config.database';
-import { ConfigService } from '@v1/config/condig.service';
 import { join } from 'path';
 import * as qiniu from 'qiniu';
-
-const configModel = new ConfigModel();
-const configService = new ConfigService(configModel);
+import { ShortDBConfig } from '../db-config';
 
 type QiniuConfig = {
     accessKey: string;
@@ -24,21 +20,35 @@ export class QiNiuService {
     private config: qiniu.conf.Config;
     private mac: qiniu.auth.digest.Mac;
     private bucket: string;
-    private dbConfigPending: Promise<unknown>;
+    private callBackInline: boolean;
     private callbackUrl: string;
+    private readonly shortDBConfig = new ShortDBConfig<QiniuConfig>([
+        1, 2, 3, 4, 5, 6, 13,
+    ]);
 
-    constructor() {
-        this.dbConfigPending = this.init();
+    get shortConfig() {
+        return this.shortDBConfig.config;
     }
 
     private async init() {
-        const config = await configService.withOsConfig<QiniuConfig>([
-            1, 2, 3, 4, 5, 6, 13,
-        ]);
+        const config = await this.shortConfig;
         this.bucket = config.bucket;
         this.callbackUrl = config.callbackUrl;
+
+        this.checkCallbackInline();
         this.initConfig(config);
         this.initMac(config);
+    }
+
+    private async checkCallbackInline() {
+        const { callbackUrl } = this;
+        try {
+            await fetch(callbackUrl, {
+                method: 'HEADER',
+            });
+        } catch {
+            throw new Error(`七牛雲圖像上傳成功回調地址異常 ${callbackUrl}`);
+        }
     }
 
     private initMac({ accessKey, secretKey }: QiniuConfig) {
@@ -78,7 +88,7 @@ export class QiNiuService {
     }
 
     async uploadByLocal(file: string, option?: qiniu.rs.PutPolicyOptions) {
-        await this.dbConfigPending;
+        await this.init();
         const localFile = join(staticConfig.root, file);
         const uploadToken = this.getUploadToken(option);
         const formUploader = this.uploader;
